eb858a4c3c2c5ea90727f73a2eed499edb74bd43
Marcel Subirana <msubiranac@gmail.com>
2021-02-04 19:56:54 +0100

    feat(checkout button): add styling to cartdropdown checkout button

--- src/components/cart-dropdown/Cart-dropdown.component.tsx
+++ src/components/cart-dropdown/Cart-dropdown.component.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../contexts/Cart.context';
 import './Cart-dropdown.styles.scss';
 
 const CartDropdown: React.FC = () => {
+  const { toggleHidden } = useContext(CartContext);
+
   return (
-    <div className="cart-dropdown">
+    <div className="cart-dropdown" onMouseLeave={toggleHidden}>
       <div className="cart-items" />
-      <button type="button" className="button button--primary">
+      <button type="button" className="button button--primary button--checkout">
         Go to checkout
       </button>
     </div>
