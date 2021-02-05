eecc9a38a1be15493ee3343b6a77ca00a71b1dac
Marcel Subirana <msubiranac@gmail.com>
2021-02-05 18:45:56 +0100

    feat(pagination): add pagination container and component logic and styles

---
+++ src/components/pagination/Pagination.component.tsx
@@ -0,0 +1,117 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import './Pagination.styles.scss';
+import { ReactComponent as ArrowLeftIcon } from '../../assets/svg/arrow-left.svg';
+import { ReactComponent as ArrowRightIcon } from '../../assets/svg/arrow-right.svg';
+
+export interface PaginationProps {
+  page: number;
+  totalPages: number;
+  // eslint-disable-next-line no-unused-vars
+  handlePagination: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ page, totalPages, handlePagination }) => {
+  console.log(totalPages);
+
+  return (
+    <nav className="pagination">
+      <ul className="pagination__list">
+        <li className="pagination__item">
+          {page !== 1 && (
+            <button
+              type="button"
+              className="pagination__button pagination__button--arrow"
+              onClick={() => handlePagination(page - 1)}
+            >
+              <ArrowLeftIcon>
+                <title>Arrow Left</title>
+              </ArrowLeftIcon>
+            </button>
+          )}
+        </li>
+
+        <li className="pagination__item">
+          {page > 2 && (
+            <button
+              type="button"
+              onClick={() => handlePagination(1)}
+              className="pagination__button"
+            >
+              {' '}
+              1{' '}
+            </button>
+          )}
+        </li>
+        <li className="pagination__item">
+          {page > 2 && (
+            <span className="pagination__button pagination__button--separator"> ... </span>
+          )}
+        </li>
+        <li className="pagination__item">
+          {page > 1 && (
+            <button
+              type="button"
+              className="pagination__button"
+              onClick={() => handlePagination(page - 1)}
+            >
+              {` ${page - 1} `}
+            </button>
+          )}
+        </li>
+        <li className="pagination__item">
+          <button
+            type="button"
+            className="pagination__button active"
+            onClick={() => handlePagination(page)}
+          >
+            {` ${page} `}
+          </button>
+        </li>
+        <li className="pagination__item">
+          {page < totalPages && (
+            <button
+              type="button"
+              className="pagination__button"
+              onClick={() => handlePagination(page + 1)}
+            >
+              {` ${page + 1} `}
+            </button>
+          )}
+        </li>
+        <li className="pagination__item">
+          {page < totalPages - 1 && (
+            <span className="pagination__button pagination__button--separator"> ... </span>
+          )}
+        </li>
+        <li className="pagination__item">
+          {page < totalPages - 1 && (
+            <button
+              type="button"
+              className="pagination__button"
+              onClick={() => handlePagination(totalPages)}
+            >
+              {` ${totalPages} `}
+            </button>
+          )}
+        </li>
+
+        <li className="pagination__item">
+          {page < totalPages && (
+            <button
+              type="button"
+              className="pagination__button pagination__button--arrow"
+              onClick={() => handlePagination(page + 1)}
+            >
+              <ArrowRightIcon>
+                <title>Arrow Right</title>
+              </ArrowRightIcon>
+            </button>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- src/components/product-list/ProductList.component.tsx
+++ src/components/product-list/ProductList.component.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './ProductList.styles.scss';
-import { ReactComponent as ArrowLeftIcon } from '../../assets/svg/arrow-left.svg';
-import { ReactComponent as ArrowRightIcon } from '../../assets/svg/arrow-right.svg';
 import ProductItem from '../product-item/ProductItem.component';
 import itemData from '../../item.data.js';
+import PaginationContainer from '../../containers/PaginationContainer';
 
-const ProductList: React.FC = () => {
+const ProductList = () => {
   return (
     <div className="container">
       <ul className="product-list">
@@ -16,54 +15,7 @@ const ProductList: React.FC = () => {
         ))}
       </ul>
 
-      <nav className="pagination">
-        <ul className="pagination__list">
-          <li className="pagination__item">
-            <a href="/" className="pagination__link">
-              <ArrowLeftIcon>
-                <title>Arrow Left</title>
-              </ArrowLeftIcon>
-            </a>
-          </li>
-          <li className="pagination__item">
-            <a href="/" className="pagination__link">
-              {' '}
-              1{' '}
-            </a>
-          </li>
-          <li className="pagination__item">
-            <a href="/" className="pagination__link">
-              {' '}
-              2{' '}
-            </a>
-          </li>
-          <li className="pagination__item">
-            <a href="/" className="pagination__link">
-              {' '}
-              3{' '}
-            </a>
-          </li>
-          <li className="pagination__item">
-            <a href="/" className="pagination__link">
-              {' '}
-              ...{' '}
-            </a>
-          </li>
-          <li className="pagination__item">
-            <a href="/" className="pagination__link">
-              {' '}
-              10{' '}
-            </a>
-          </li>
-          <li className="pagination__item">
-            <a href="/" className="pagination__link">
-              <ArrowRightIcon>
-                <title>Arrow Right</title>
-              </ArrowRightIcon>
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <PaginationContainer />
     </div>
   );
 };
